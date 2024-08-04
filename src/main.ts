70214cd0bac0dfd882863503a86b6717f85c643a
SkerryRamble <168703420+SkerryRamble@users.noreply.github.com>
2024-08-04 22:26:07 +0200

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,59 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {Component} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+interface Task { done: boolean; description: string; nestedTask?: Task;}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <h2>Liosta Tascanna</h2>
+    <input #text />
+    <button (click)="add(text.value)">Add</button>
+
+    @for (task of tasks; track $index)
+	{
+		<p>
+		  <input type="checkbox" (change)="toggle($index)" />
+      <input type="button" (click)="delete($index)" value="X"/>
+      <input type="button" (click)="nest()" value="+"/>
+      
+      @if (task.done) {
+        <s>{{ task.description}}</s>
+
+      } @else {
+        <span>{{ task.description}}</span>
+      }
+
+      <!-- check for nested list -->
+      
+		</p>
+    }
+	@empty
+	{
+		<p>No todos</p>
+    }
+  `,
+})
+
+export class TodosComponent {
+  tasks: Task[] = [];
+
+  add(description: string) {
+    this.tasks.push({description, done: false});
+  }
+
+  toggle(index: number) {
+    this.tasks[index].done = !this.tasks[index].done;
+  }
+
+  delete(index: number): void {console.log(index);
+    this.tasks.splice(index, 1);
+  }
+
+  nest(): void {
+    // todo: recursively add the todo input box and add button?
+  }
+}
+
+bootstrapApplication(TodosComponent);
